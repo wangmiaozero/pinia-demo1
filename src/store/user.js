8ddaa4f2d472b14f4d884c1005e445b9548f867f
wangmiao <1061214467@qq.com>
2022-04-09 22:59:19 +0800

    fix

--- src/store/user.js
+++ src/store/user.js
@@ -15,41 +15,40 @@ export const useUserStore = defineStore({
         // ]
         strategies: [
             {
-              storage: localStorage,
-              paths: ['name', 'age']
+                storage: localStorage,
+                paths: ['name', 'age']
             }
-          ]
+        ]
     },
     state: () => {
-      return {
-        name: '张三',
-        age: 18,
-        gender: '男'
-      }
+        return {
+            name: '张三',
+            age: 18,
+            gender: '男'
+        }
     },
     getters: {
         fullName: (state) => {
-          return state.name + '丰'
+            return state.name + '丰'
         }
-      },
+    },
     actions: {
-      updateName(name) {
-        this.name = name
-      },
-      async login(account, pwd) {
-        Promise.all([API.getOnlinePerson(account, pwd),API.getRegPerson()]).then(function([onlinePerson,RegPerson]){
-            //这里写等这两个ajax都成功返回数据才执行的业务逻辑
-            API.calOnlinePercent(onlinePerson.onlinePerson,RegPerson).then(function(percent){
-                const appStore = useAppStore()
-                let data = { percent,...onlinePerson }
-                appStore.setData(data) // 调用 app store 里的 action 方法
-                return data
+        updateName(name) {
+            this.name = name
+        },
+        async login(account, pwd) {
+            Promise.all([API.getOnlinePerson(account, pwd), API.getRegPerson()]).then(function ([onlinePerson, RegPerson]) {
+                //这里写等这两个ajax都成功返回数据才执行的业务逻辑
+                API.calOnlinePercent(onlinePerson.onlinePerson, RegPerson).then(function (percent) {
+                    const appStore = useAppStore()
+                    let data = { percent, ...onlinePerson }
+                    appStore.setData(data) // 调用 app store 里的 action 方法
+                    return data
+                })
             })
-        })
-       
-        
-      }
+
+        }
     }
-  })
-  
+})
+
 
